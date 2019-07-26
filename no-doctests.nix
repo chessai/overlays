15c75cfac5359f35c2c7d1d84216ecc2370ebc66
chessai <chessai1996@gmail.com>
2019-07-26 15:13:05 -0400

    add no-doctests.nix

---
+++ no-doctests.nix
@@ -0,0 +1,23 @@
+{ hlib # haskell.lib
+}:
+
+hself: hsuper: {
+  comonad = hlib.disableCabalFlag hsuper.comonad "test-doctests";
+  semigroupoids = hlib.disableCabalFlag hsuper.semigroupoids "doctests";
+  lens = hlib.disableCabalFlag hsuper.lens "test-doctests";
+  distributive = hlib.dontCheck (hlib.disableCabalFlag hsuper.distributive "test-doctests");
+
+  http-types        = hlib.dontCheck hsuper.http-types;
+  silently          = hlib.dontCheck hsuper.silently;
+  unliftio          = hlib.dontCheck hsuper.unliftio;
+  conduit           = hlib.dontCheck hsuper.conduit;
+  yaml              = hlib.dontCheck hsuper.yaml;
+  extra             = hlib.dontCheck hsuper.extra;
+  half              = hlib.dontCheck hsuper.half;
+  iproute           = hlib.dontCheck hsuper.iproute;
+  aeson-compat      = hlib.dontCheck hsuper.aeson-compat;
+  tzdata            = hlib.dontCheck hsuper.tzdata;
+  tz                = hlib.dontCheck hsuper.tz;
+  time-exts         = hlib.dontCheck hsuper.time-exts;
+  double-conversion = hlib.dontCheck hsuper.double-conversion;
+}
